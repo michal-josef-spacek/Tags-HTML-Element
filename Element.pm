f328580d776751c8a25c4643499534c90e41c09d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-01-05 12:31:50 +0100

    Add Textarea, examples

---
+++ Element.pm
@@ -0,0 +1,50 @@
+package Tags::HTML::Element;
+
+use strict;
+use warnings;
+
+our $VERSION = 0.01;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Data::HTML::Element - Data objects for HTML elements.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Tags::HTML::Element::Button>
+
+TODO
+
+=back
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Tags-HTML-Element>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2022-2024 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
