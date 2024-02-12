e06ed293565ad0f09de844f94be9bc4b00abf9d9
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-02-12 12:18:57 +0100

    Prepare for 0.08 version

--- Element.pm
+++ Element.pm
@@ -3,7 +3,7 @@ package Tags::HTML::Element;
 use strict;
 use warnings;
 
-our $VERSION = 0.07;
+our $VERSION = 0.08;
 
 1;
 
@@ -69,6 +69,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.07
+0.08
 
 =cut
